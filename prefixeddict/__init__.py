6e511de8bd599dad0d4cbfb932ce3a794b29cd65
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2016-10-28 15:32:35 -0200

    Improved stability, laziness and compatibility.

--- prefixeddict/__init__.py
+++ prefixeddict/__init__.py
@@ -1,3 +1,10 @@
+try:
+    from itertools import izip
+    lazyzip = izip
+except ImportError:
+    lazyzip = zip
+
+
 class PrefixedDict(object):
     """
     Prefixed dictionary, makes all keys be automatically prefixed with a given
@@ -9,24 +16,32 @@ class PrefixedDict(object):
         Args:
             prefix (str): the prefix you want to apply to the dictionary's
                           keys.
-            dictionary (dict-like): the actual dictionary-like object which will
-                                    be used to store data.
+            dictionary (dict-like): the actual dictionary-like object which
+                                    will be used to store data.
         """
 
-        self.prefix = prefix + '-'
         self.dictionary = dictionary
+        self.prefix = prefix
 
     def __setitem__(self, key, value):
-        self.dictionary[self.prefix + key] = value
+        self.dictionary[self.prefix + '-' + key] = value
 
     def __getitem__(self, key):
-        return self.dictionary[self.prefix + key]
+        return self.dictionary[self.prefix + '-' + key]
+
+    def __contains__(self, key):
+        return (self.prefix + '-' + key) in self.dictionary
 
     def keys(self):
-        return [key.replace(self.prefix, '') for key in self.dictionary.keys()]
+        for key in self.dictionary.keys():
+            if key.startswith(self.prefix + '-'):
+                yield key.replace(self.prefix + '-', '')
 
     def values(self):
-        return self.dictionary.values()
+        for key in self.keys():
+            yield self[key]
 
     def items(self):
-        return zip(self.keys(), self.values())
+        return lazyzip(self.keys(), self.values())
+
+    __iter__ = keys
--- tests.py
+++ tests.py
@@ -4,9 +4,6 @@ from prefixeddict import PrefixedDict
 
 
 class TestPrefixedDict(unittest.TestCase):
-    def setUp(self):
-        self.filled_dictionary = {'age': 30, 'name': 'john'}
-
     def test_dictionary_same_object(self):
         """
         Make sure whatever dictionary is passed to PrefixedDict is not a
@@ -20,34 +17,113 @@ class TestPrefixedDict(unittest.TestCase):
 
     def test_prefixing(self):
         dictionary = {}
-        prefixed = PrefixedDict('prefix-1', dictionary)
+        prefixed = PrefixedDict('prefix', dictionary)
         prefixed['key'] = 'value'
 
         self.assertEqual(prefixed['key'], 'value')
-        self.assertEqual(dictionary['prefix-1-key'], 'value')
+        self.assertEqual(dictionary['prefix-key'], 'value')
+
+    def test_prefix_attr(self):
+        """
+        Ensure the prefix's suffix(the hyphen) isn't added to the
+        attribute itself when __init__ is called.
+        """
+
+        prefixed = PrefixedDict('prefix', {})
+        self.assertEqual(prefixed.prefix, 'prefix')
 
     def test_keys(self):
-        """Keys should only be prefixed internally."""
+        dictionary = {'age': 30, 'name': 'john'}
 
-        prefixed = PrefixedDict('prefix', self.filled_dictionary)
+        bar_prefixed = PrefixedDict('bar', dictionary)
+        foo_prefixed = PrefixedDict('foo', dictionary)
 
-        self.assertEqual(set(prefixed.keys()),
-                         set(self.filled_dictionary.keys()))
+        self.assertEqual(set(foo_prefixed.keys()), set([]))
+        self.assertEqual(set(bar_prefixed.keys()), set([]))
+
+        foo_prefixed['gender'] = 'male'
+        foo_prefixed['name'] = 'peter'
+
+        self.assertEqual((set(foo_prefixed.keys())), set(['gender', 'name']))
+        self.assertEqual(set(bar_prefixed.keys()), set([]))
+
+        bar_prefixed['country'] = 'germany'
+        bar_prefixed['email'] = 'peter@gmail.com'
+
+        self.assertEqual(set(bar_prefixed.keys()), set(['country', 'email']))
+        self.assertEqual(set(foo_prefixed.keys()), set(['gender', 'name']))
+
+        self.assertEqual(set(dictionary.keys()),
+                         set(['age', 'name',
+                              'foo-gender', 'foo-name',
+                              'bar-country', 'bar-email']))
 
     def test_values(self):
-        prefixed = PrefixedDict('prefix', self.filled_dictionary)
+        dictionary = {'age': 30, 'name': 'john'}
 
-        self.assertEqual(set(prefixed.values()),
-                         set(self.filled_dictionary.values()))
+        bar_prefixed = PrefixedDict('bar', dictionary)
+        foo_prefixed = PrefixedDict('foo', dictionary)
+
+        self.assertEqual(set(foo_prefixed.values()), set([]))
+        self.assertEqual(set(bar_prefixed.values()), set([]))
+
+        foo_prefixed['country'] = 'germany'
+
+        self.assertEqual(set(foo_prefixed.values()), set(['germany']))
+        self.assertEqual(set(bar_prefixed.values()), set([]))
+
+        bar_prefixed['email'] = 'peter@gmail.com'
+
+        self.assertEqual(set(bar_prefixed.values()), set(['peter@gmail.com']))
+        self.assertEqual(set(foo_prefixed.values()), set(['germany']))
 
     def test_items(self):
-        prefixed = PrefixedDict('prefix', self.filled_dictionary)
+        dictionary = {'age': 30, 'name': 'john'}
 
-        self.assertEqual(set(prefixed.items()),
-                         set(self.filled_dictionary.items()))
+        bar_prefixed = PrefixedDict('bar', dictionary)
+        foo_prefixed = PrefixedDict('foo', dictionary)
+
+        self.assertEqual(set(bar_prefixed.items()), set([]))
+        self.assertEqual(set(foo_prefixed.items()), set([]))
+
+        bar_prefixed['country'] = 'germany'
+        bar_prefixed['email'] = 'peter@gmail.com'
+
+        self.assertEqual(set(bar_prefixed.items()),
+                         set([('country', 'germany'),
+                              ('email', 'peter@gmail.com')]))
+        self.assertEqual(set(foo_prefixed.items()), set([]))
+
+        foo_prefixed['age'] = 35
+        foo_prefixed['name'] = 'peter'
+
+        self.assertEqual(set(foo_prefixed.items()),
+                         set([('age', 35),
+                              ('name', 'peter')]))
 
     def test_keyerror(self):
-        prefixed = PrefixedDict('prefix', {})
+        dictionary = {'email': 'peter@gmail.com'}
+
+        prefixed = PrefixedDict('prefix', dictionary)
 
         with self.assertRaises(KeyError):
-            prefixed['invalid-key']
+            prefixed['email']
+
+    def test_in(self):
+        dictionary = {'email': 'peter@gmail.com'}
+
+        prefixed = PrefixedDict('prefix', dictionary)
+
+        self.assertFalse('email' in prefixed)
+
+        prefixed['name'] = 'peter'
+
+        self.assertTrue('name' in prefixed)
+
+    def test_iteration_on_keys(self):
+        prefixed = PrefixedDict('prefix', {})
+        prefixed['name'] = 'peter'
+        prefixed['email'] = 'peter@gmail.com'
+
+        self.assertEqual(set(prefixed.keys()),
+                         set([key for key in prefixed]))
